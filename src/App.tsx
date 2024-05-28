648f0333add0eeb2d95dbc356ad7dbb816fa50b7
FullTimeScam <418.gingin@gmail.com>
2024-05-28 16:50:49 +0900

    프롭스 사용하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
+import Box from "./components/Box";
 
 const App: FC = () => {
-  return <div className="bg-red-100">Hello, ReactTS!</div>;
+  return (
+    <div className="bg-red-100 min-h-screen flex flex-col justify-center items-center gap-10">
+      <Box name="바나나 박스" bgColor="bg-yellow-500" isRounded={false} />
+      <Box name="사과 박스" bgColor="bg-red-500" isRounded={true} />
+      <Box name="컨테이너 박스" bgColor="bg-gray-500" isRounded={false} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Box.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+type BoxProps = {
+  name: string;
+  bgColor: string;
+  isRounded?: boolean;
+};
+
+const Box: FC<BoxProps> = ({ name, bgColor, isRounded = false }) => {
+  return (
+    <div
+      className={`${bgColor} ${isRounded && "rounded-full"}
+      w-96
+      h-96
+      text-3xl
+      font-bold
+      flex justify-center items-center`}
+    >
+      {name}
+    </div>
+  );
+};
+
+export default Box;
